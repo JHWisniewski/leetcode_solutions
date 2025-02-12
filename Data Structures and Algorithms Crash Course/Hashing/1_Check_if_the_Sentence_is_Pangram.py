a2af20356cec27f943520223fc55ce313715c4fe
Xetanees <xetanees@gmail.com>
2025-02-11 22:31:58 -0500

    Check_if_the_Sentence_is_Pangram

---
+++ Data Structures and Algorithms Crash Course/Hashing/1_Check_if_the_Sentence_is_Pangram.py
@@ -0,0 +1,46 @@
+import json
+import string
+
+class Solution(object):
+    """
+    Description:
+        A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+        Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+    Constraints:
+        1 <= sentence.length <= 1000
+        sentence consists of lowercase English letters.
+    """
+    def checkIfPangram(self, sentence):
+        """
+        :type sentence: str
+        :rtype: bool
+        """
+        letters = set(string.ascii_lowercase)
+        s_letters = set(sentence)
+
+        return s_letters == letters
+
+
+def main():
+    #Setup of solution with LeetCode example input
+    solution = Solution()
+    
+    with open('./Problem Inputs/1_Input.json') as f:
+        JSON = json.loads(f.read())
+
+    for object in JSON:
+        print(f"Input: sentence = {object['sentence']}")
+        output = solution.checkIfPangram(object['sentence'])
+        print(f"Output: {output}")
+        print(f"Expected Output: {object['expected']}")
+
+        if output == object['expected']:
+            print("PASS")
+        else:
+            print("FAIL")
+        print()
+
+if __name__ == "__main__":
+    main()
