c9aa749cdfb27fd9d078a79f21829bdd790e868a
Xetanees <xetanees@gmail.com>
2025-02-22 00:23:14 -0500

    217_contains_duplicate

---
+++ data_structures_and_algorithms/hashing/addtl_probs/217_contains_duplicate/contains_duplicate.py
@@ -0,0 +1,43 @@
+import json
+from lib.test import test
+
+
+class Solution(object):
+    """
+    Description:
+        Given an integer array nums, return true if any value appears at least twice in the array,
+        and return false if every element is distinct.
+
+    Constraints:
+        1 <= nums.length <= 10^5
+        -10^9 <= nums[i] <= 10^9
+
+    """
+
+    def contains_duplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) == len(set(nums)):
+            return False
+
+        return True
+
+
+def main():
+    # Setup of solution with LeetCode example input
+    solution = Solution()
+    path = "./data_structures_and_algorithms/hashing/addtl_probs/217_contains_duplicate/input.json"
+
+    with open(path, encoding="utf-8") as f:
+        data = json.loads(f.read())
+
+    for item in data:
+        print(f"Input: nums = {item['nums']}")
+        output = solution.contains_duplicate(item["nums"])
+        test(output, item["expected"])
+
+
+if __name__ == "__main__":
+    main()
